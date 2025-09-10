e125d31b026e2acebe112040edb96200498f605c
hello23241 <nhankiet1705@gmail.com>
2025-09-10 16:46:08 +0700

    Added the baseline system. Clicking button to earn points, use points to upgrade for more points and then prestige. Prestige scale is a little too small, will change later.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,8 +32,107 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
+
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelPoint = new System.Windows.Forms.Label();
+            this.buttonUpgrade = new System.Windows.Forms.Button();
+            this.labelUpgradeCost = new System.Windows.Forms.Label();
+            this.buttonPrestige = new System.Windows.Forms.Button();
+            this.labelPrestigeCost = new System.Windows.Forms.Label();
+            this.labelUpgradeInfo = new System.Windows.Forms.Label();
+            this.buttonDebug = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.Text = "Click Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelPoint
+            // 
+            this.labelPoint.Location = new System.Drawing.Point(140, 30);
+            this.labelPoint.Name = "labelPoint";
+            this.labelPoint.Size = new System.Drawing.Size(80, 30);
+            this.labelPoint.Text = "0";
+            this.labelPoint.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonUpgrade
+            // 
+            this.buttonUpgrade.Location = new System.Drawing.Point(30, 70);
+            this.buttonUpgrade.Name = "buttonUpgrade";
+            this.buttonUpgrade.Size = new System.Drawing.Size(100, 30);
+            this.buttonUpgrade.Text = "Upgrade";
+            this.buttonUpgrade.UseVisualStyleBackColor = true;
+            this.buttonUpgrade.Click += new System.EventHandler(this.buttonUpgrade_Click);
+            // 
+            // labelUpgradeCost
+            // 
+            this.labelUpgradeCost.Location = new System.Drawing.Point(140, 70);
+            this.labelUpgradeCost.Name = "labelUpgradeCost";
+            this.labelUpgradeCost.Size = new System.Drawing.Size(120, 30);
+            this.labelUpgradeCost.Text = "Upgrade Cost: 5";
+            this.labelUpgradeCost.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelUpgradeInfo
+            // 
+            this.labelUpgradeInfo.Location = new System.Drawing.Point(270, 70);
+            this.labelUpgradeInfo.Name = "labelUpgradeInfo";
+            this.labelUpgradeInfo.Size = new System.Drawing.Size(300, 30);
+            this.labelUpgradeInfo.Text = "improve click gain by 1 + x*(1.01+[prestige*0.01])";
+            this.labelUpgradeInfo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonPrestige
+            // 
+            this.buttonPrestige.Location = new System.Drawing.Point(30, 110);
+            this.buttonPrestige.Name = "buttonPrestige";
+            this.buttonPrestige.Size = new System.Drawing.Size(100, 30);
+            this.buttonPrestige.Text = "Prestige";
+            this.buttonPrestige.UseVisualStyleBackColor = true;
+            this.buttonPrestige.Click += new System.EventHandler(this.buttonPrestige_Click);
+            // 
+            // labelPrestigeCost
+            // 
+            this.labelPrestigeCost.Location = new System.Drawing.Point(140, 110);
+            this.labelPrestigeCost.Name = "labelPrestigeCost";
+            this.labelPrestigeCost.Size = new System.Drawing.Size(120, 30);
+            this.labelPrestigeCost.Text = "Prestige Cost: 1000";
+            this.labelPrestigeCost.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // buttonDebug
+            // 
+            this.buttonDebug.Location = new System.Drawing.Point(670, 390);
+            this.buttonDebug.Name = "buttonDebug";
+            this.buttonDebug.Size = new System.Drawing.Size(100, 40);
+            this.buttonDebug.Text = "Debug x10";
+            this.buttonDebug.UseVisualStyleBackColor = true;
+            this.buttonDebug.Click += new System.EventHandler(this.buttonDebug_Click);
+            // 
+            // Form1
+            // 
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelPoint);
+            this.Controls.Add(this.buttonUpgrade);
+            this.Controls.Add(this.labelUpgradeCost);
+            this.Controls.Add(this.labelUpgradeInfo);
+            this.Controls.Add(this.buttonPrestige);
+            this.Controls.Add(this.labelPrestigeCost);
+            this.Controls.Add(this.buttonDebug);
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelPoint;
+        private System.Windows.Forms.Button buttonUpgrade;
+        private System.Windows.Forms.Label labelUpgradeCost;
+        private System.Windows.Forms.Button buttonPrestige;
+        private System.Windows.Forms.Label labelPrestigeCost;
+        private System.Windows.Forms.Label labelUpgradeInfo;
+        private System.Windows.Forms.Button buttonDebug;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,70 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private int point = 0;
+        private double pointMultiplier = 1.0;
+        private double upgradeCost = 5.0;
+        private double prestigeBonus = 0.0;
+        private double prestigeCost = 1000.0;
+
         public Form1()
         {
             InitializeComponent();
+            labelPoint.Text = point.ToString();
+            labelUpgradeCost.Text = $"Upgrade Cost: {upgradeCost:F0}";
+            button1.Text = $"+{(int)pointMultiplier} points";
+            UpdateUpgradeInfoLabel();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            point += (int)pointMultiplier;
+            labelPoint.Text = point.ToString();
+            button1.Text = $"+{(int)pointMultiplier} points";
+        }
+
+        private void buttonUpgrade_Click(object sender, EventArgs e)
+        {
+            if (point >= (int)upgradeCost)
+            {
+                point -= (int)upgradeCost;
+                pointMultiplier = 1 + pointMultiplier * (1.01 + prestigeBonus);
+                upgradeCost = Math.Pow(upgradeCost, 1.05);
+                labelPoint.Text = point.ToString();
+                labelUpgradeCost.Text = $"Upgrade Cost: {upgradeCost:F0}";
+                button1.Text = $"+{(int)pointMultiplier} points";
+                UpdateUpgradeInfoLabel();
+            }
+        }
+
+        private void buttonPrestige_Click(object sender, EventArgs e)
+        {
+            if (point >= (int)prestigeCost)
+            {
+                point = 0;
+                prestigeBonus += 0.02;
+                pointMultiplier = 1.0 + prestigeBonus;
+                upgradeCost = 5.0;
+                prestigeCost = Math.Pow(prestigeCost, 1.05);
+                labelPrestigeCost.Text = $"Prestige Cost: {prestigeCost:F0}";
+                labelPoint.Text = point.ToString();
+                labelUpgradeCost.Text = $"Upgrade Cost: {upgradeCost:F0}";
+                button1.Text = $"+{(int)pointMultiplier} points";
+                UpdateUpgradeInfoLabel();
+            }
+        }
+
+        private void buttonDebug_Click(object sender, EventArgs e)
+        {
+            pointMultiplier *= 10;
+            button1.Text = $"+{(int)pointMultiplier} points";
+            UpdateUpgradeInfoLabel();
+        }
+
+        private void UpdateUpgradeInfoLabel()
+        {
+            double multiplier = 1.01 + prestigeBonus;
+            labelUpgradeInfo.Text = $"improve click gain by 1 + x*({multiplier:F2})";
         }
     }
 }
